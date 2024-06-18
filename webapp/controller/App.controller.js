73e43439d76547f0e7071a5a37d5a21faa5b70b2
Abongile <abongilewilliam@gmail.com>
2024-06-18 11:49:53 +0200

    modules and Json Model covered

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
- ], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+ ], (Controller, MessageToast, JSONModel) => {
     "use strict";
  
     return Controller.extend("ui5.quickstart.controller.App", {
+
+      onInit() {
+         //set the data model on the view 
+
+         var oData = {
+            recipient : {
+               name: "UI5"
+            }
+         };
+
+         var oModel = new JSONModel(oData);
+         this.getView().setModel(oModel); 
+      },
+
        onShowHello() {
+
+
           // show a native JavaScript alert
-          alert("Hello World");
+          MessageToast.show("Hello there")
        }
     });
  });
